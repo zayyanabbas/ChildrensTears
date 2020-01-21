9a46215a13839b209a46bb716f50916e82562e09
Zayyan Abbas <zayyan_abbas@pop-os.localdomain>
2020-01-21 17:27:31 +0600

    Added BaseEntity class

---
+++ Headers/Entities.hpp
@@ -0,0 +1,22 @@
+#include "ECS/Coordinator.hpp"
+#include "Components.hpp"
+#pragma once
+
+extern ChildrensTears::Coordinator coord;
+
+namespace ChildrensTears {
+    class BaseEntity {
+    protected:
+        uint32_t id;
+        void addEntity() {
+            coord.createEntity();
+            id = coord.getLatestID()-1;
+        }
+    public:
+        virtual void init() = 0;
+        virtual void update(float = 0) = 0;
+
+        virtual void onCollision(RigidbodyComponent comp) {}
+        virtual void setPosition(Position pos) {}
+    };
+}
