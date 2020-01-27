f23c99889bd6f5ac5d9d8e3d56f4bcd0ab1e2ccf
Zayyan Abbas <zayyan_abbas@pop-os.localdomain>
2020-01-27 23:50:01 +0600

    Removed resultant force from physics component

--- Headers/Components.hpp
+++ Headers/Components.hpp
@@ -6,21 +6,15 @@
 
 namespace ChildrensTears {
     struct PhysicsComponent {
-        Vec2<float> resultantForce;
-        Vec2<float> acceleration;
-        Vec2<float> velocity;
-
-        PhysicsComponent() {
-            resultantForce = Vec2<float>(0,0);
-            acceleration   = Vec2<float>(0,0);
-            velocity       = Vec2<float>(0,0);
-        }
-
+        Vec2<float> acceleration{0,0};
+        Vec2<float> velocity{0,0};
+    
         float mass = 1.0;
         uint32_t id;
     };
 
     struct RigidbodyComponent {
+        bool isColliding = false;
         bool hasGravity = false;
         bool isStatic = false;
 
@@ -34,7 +28,7 @@ namespace ChildrensTears {
             scale    = Vec2<float>(1,1);
         }
 
-        float g_accel = 0.5;
+        constexpr static float g_accel = 1;
         float angle;
         float mass = 1;
 
