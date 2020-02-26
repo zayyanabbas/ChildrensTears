4bc1b9f3495eccd7c95aa5cdf1ee7af4afb9ff60
Zayyan Abbas <zayyan_abbas@pop-os.localdomain>
2020-02-26 19:56:04 +0600

    Added frustrum culling

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -8,14 +8,15 @@ extern ChildrensTears::EntityRegistry entity_registry;
 namespace ChildrensTears {
     Game::Game() {
         // Register parts of the engine
+        rigidbody_system = coord.registerSystem<RigidbodySystem>();
+        physics_system = coord.registerSystem<PhysicsSystem>();
+        render_system = coord.registerSystem<RenderSystem>();
+        transform_system = coord.registerSystem<TransformSystem>();
+
         coord.registerComponent<RigidbodyComponent>();
         coord.registerComponent<PhysicsComponent>();
         coord.registerComponent<RenderComponent>();
         coord.registerComponent<TransformComponent>();
-
-        rigidbody_system = coord.registerSystem<RigidbodySystem>();
-        physics_system = coord.registerSystem<PhysicsSystem>();
-        render_system = coord.registerSystem<RenderSystem>();
     }
 
     void Game::loop() {
@@ -29,35 +30,35 @@ namespace ChildrensTears {
             ent->update(deltaTime);
         }
 
-        // Run all the loops of the systems
-        physics_system->update(deltaTime);
-        rigidbody_system->update(deltaTime);
-
         // What the window can see right now
         AABB windowFrame{{renderTarget->getView().getCenter().x - renderTarget->getView().getSize().x/2,
                           renderTarget->getView().getCenter().y - renderTarget->getView().getSize().y/2},
                           {renderTarget->getView().getSize().x, renderTarget->getView().getSize().y}};
 
-        rigidbody_system->setScreen(windowFrame);
+        transform_system->setScreen(windowFrame);
+
+        transform_system->update();
 
         // Check collision
 
         // For all the ones currently in the window
-        for (auto& ent : rigidbody_system->getInRange(windowFrame)) {
+        for (auto& ent_id : transform_system->getInRange(windowFrame)) {
+            // Run all the loops of the systems
+            physics_system->update(ent_id, deltaTime);
+            rigidbody_system->update(ent_id,deltaTime);
+            render_system->drawRenderable(ent_id,renderTarget);
+
             // Check all the ones their colliding against
             // And run onCollision
-            for (auto& collider : rigidbody_system->checkCollision(windowFrame, *ent.id)) {
-                rigidbody_system->onCollision(collider, *ent.id);
-                if (entity_registry.getEntity(*ent.id) != nullptr) { 
-                    entity_registry.getEntity(*ent.id)->onCollision(collider);
+            for (auto& collider : rigidbody_system->checkCollision(windowFrame, ent_id, transform_system->getInRange(windowFrame))) {
+                if (entity_registry.getEntity(ent_id) != nullptr) {
+                    rigidbody_system->onCollision(collider, ent_id);
+                    entity_registry.getEntity(ent_id)->onCollision(collider);
                 }
             }
 
-            physics_system->applyCollision(*ent.id, deltaTime);
+            physics_system->applyCollision(ent_id, deltaTime);
         }
-
-        
-        render_system->drawRenderables(renderTarget);
 
         // Take time at the end of the frame
         auto end = std::chrono::high_resolution_clock::now();
